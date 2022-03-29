a0a8e046c8ad67f99e183ddf85cccc38a2a5f865
DESKTOP-NIP6QJC\서예원 <syw001123@naver.com>
2022-03-29 11:39:12 +0900

    make toolbar

--- app/src/main/java/com/example/withwheel/MainActivity.java
+++ app/src/main/java/com/example/withwheel/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.withwheel;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
+
+    private ImageView ivMenu;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivMenu=findViewById(R.id.iv_menu);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolbar);
+
+        //액션바 변경하기(들어갈 수 있는 타입 : Toolbar type
+        setSupportActionBar(toolbar);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d(TAG, "onClick: 클릭됨"); //이게 뭘까
+                drawerLayout.openDrawer(Gravity.RIGHT);
+            }
+        });
     }
 }
