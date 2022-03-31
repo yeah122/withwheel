ac96c9ab8c015705901811859b5cb9de9ab24bec
wooyu <wooyun0423@naver.com>
2022-03-31 12:24:37 +0900

    로그인화면, 회원가입 화면

--- app/src/main/java/com/example/withwheel/MainActivity.java
+++ app/src/main/java/com/example/withwheel/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.withwheel;
 
-import androidx.appcompat.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ImageView;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main_Activity";
@@ -21,9 +22,11 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         ivMenu=findViewById(R.id.iv_menu);
         drawerLayout=findViewById(R.id.drawer);
@@ -40,4 +43,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    Button button = (Button) findViewById(R.id.button);
+    button.
+
+    public void onButton1clicked (View v){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
+    public void onButton2Clicked(View v) {
+        Toast.makeText(this, "교통편 안내", Toast.LENGTH_SHORT).show();
+    }
+    public void onButton3Clicked(View v) {
+        Toast.makeText(this, "급속충전기 위치", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://data.seoul.go.kr/dataList/OA-15813/S/1/datasetView.do"));
+        startActivity(intent);
+    }
+
 }
